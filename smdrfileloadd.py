fae064c28fca7f5e2684a4bb143a3b60e1476b22
Denis Pavlov <pavlovdo@gmail.com>
2020-11-19 21:43:44 +0300

    more changes in main file

--- smdrfileloadd.py
+++ smdrfileloadd.py
@@ -1,15 +1,20 @@
 #!/usr/bin/env python
 
-import sys, os, time, ConfigParser
+import sys
+import os
+import ConfigParser
 from daemon import Daemon
-from socket import *                                    # get socket constructor and constants
+from socket import socket, AF_INET, SOCK_STREAM
 from datetime import datetime
 
-class MyDaemon(Daemon):
-    def run(self):
-        orbconffile = '/etc/orbit/orbit.cfg'
 
+class MyDaemon(Daemon):
+
+    def run(self):
+
+        orbconffile = '/etc/orbit/orbit.cfg'
         config = ConfigParser.RawConfigParser()
+
         try:
             config.read(orbconffile)
             listenif = config.get('SMDRConfig', 'listenif')
@@ -33,43 +38,48 @@ class MyDaemon(Daemon):
         logtempfile = logfile + '.' + curweek + '.' + curday + '.' + curhour
         smdrlog = open(logdir + logtempfile, 'a')
 
-        listensock = socket(AF_INET, SOCK_STREAM)                  # make a TCP socket object
-        listensock.bind((listenif, listenport))                    # bind it to server port number
-        listensock.listen(1)                                       # listen, allow 1 pending connects
+#       make a TCP socket object
+        listensock = socket(AF_INET, SOCK_STREAM)
+#       bind it to server port number
+        listensock.bind((listenif, listenport))
+#       listen, allow 1 pending connects
+        listensock.listen(1)
 
-        while True:                                             # listen until process killed
-            avcon, address = listensock.accept()          # wait for next client connect
+#       listen until process killed
+        while True:
+            # wait for next client connect
+            avcon, address = listensock.accept()
             while True:
                 smdrdata = avcon.recv(recvbuff)
-                if not smdrdata: 
+                if not smdrdata:
                     break
                 if curhour != datetime.now().strftime('%H'):
                     smdrlog.close()
                     os.rename(logdir + logtempfile, archlogdir + logtempfile)
                     curweek = datetime.now().strftime('%W')
                     curhour = datetime.now().strftime('%H')
-                    curday = datetime.now().strftime('%w') 
-                    logtempfile = logfile + '.' + curweek + '.' + curday + '.' + curhour
-                    smdrlog = open(logdir + logtempfile, 'a') 
+                    curday = datetime.now().strftime('%w')
+                    logtempfile = logfile + '.' + curweek + '.'
+                    + curday + '.' + curhour
+                    smdrlog = open(logdir + logtempfile, 'a')
                 smdrlog.write(smdrdata)
                 smdrlog.flush()
         avcon.close()
         smdrlog.close()
 
 if __name__ == "__main__":
-	daemon = MyDaemon('/var/run/smdrfileloadd.pid')
-#        daemon = Daemon('/var/run/smdrrec.pid')
-	if len(sys.argv) == 2:
-		if 'start' == sys.argv[1]:
-			daemon.start()
-		elif 'stop' == sys.argv[1]:
-			daemon.stop()
-		elif 'restart' == sys.argv[1]:
-			daemon.restart()
-		else:
-			print "Unknown command"
-			sys.exit(2)
-		sys.exit(0)
-	else:
-		print "usage: %s start|stop|restart" % sys.argv[0]
-		sys.exit(2)
+    daemon = MyDaemon('/var/run/smdrfileloadd.pid')
+    if len(sys.argv) == 2:
+        if 'start' == sys.argv[1]:
+            daemon.start()
+        elif 'stop' == sys.argv[1]:
+            daemon.stop()
+        elif 'restart' == sys.argv[1]:
+            daemon.restart()
+        else:
+            print "Unknown command"
+            sys.exit(2)
+            sys.exit(0)
+    else:
+        print "usage: %s start|stop|restart" % sys.argv[0]
+        sys.exit(2)
